3436811b27b77603ea49047edf78beb7418f9edd
nErumin <nerumin90@gmail.com>
2018-05-05 18:38:01 +0900

    Sentry 추가.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
+import io.sentry.Sentry;
+
 public class Main {
     public static int returnOne() {
         return 1;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String sentryDSN =
+            "https://dfa5eedd277d47658bbc691394bef7e8@sentry.io/1201571";
+        Sentry.init(sentryDSN);
+
+        try {
+            System.out.println("Hello, world!");
+        } catch (Exception exception) {
+            Sentry.capture(exception);
+        }
     }
 }
