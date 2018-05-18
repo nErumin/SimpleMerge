0a92725e76451db9148b9e0b3015bc6f6a1a6f22
nErumin <nerumin90@gmail.com>
2018-05-19 02:32:38 +0900

    Line 클래스에 구현된 메서드에 대해 기초 테스트를 작성함.

---
+++ src/test/java/model/LineTest.java
@@ -0,0 +1,69 @@
+package model;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LineTest {
+    @Test(expected = NullPointerException.class)
+    public void lineCreationWithNullTest() {
+        final Line line = new Line(null);
+    }
+
+    @Test
+    public void emptyLineConvertTest() {
+        final String originalContent = "";
+        final Line line = new Line();
+        final String lineContent = line.toString();
+
+        Assert.assertThat(lineContent, is(equalTo(originalContent)));
+    }
+
+    @Test
+    public void emptyLineWordTest() {
+        final Line line = new Line();
+
+        final List<String> words = new ArrayList<>();
+        line.words().forEach(words::add);
+
+        Assert.assertThat(words.size(), is(equalTo(0)));
+    }
+
+    @Test
+    public void multiWordsLineConvertTest() {
+        final String originalContent = "Hello, \t \t World!";
+        final Line line = new Line(originalContent);
+        final String lineContent = line.toString();
+
+        Assert.assertThat(lineContent, is(equalTo(originalContent)));
+    }
+
+    @Test
+    public void multiWordsLineWordTest() {
+        final String originalContent = "Hello, \t World!\n \r\t \n\n \t\r\n";
+        final Line line = new Line(originalContent);
+
+        final List<String> words = new ArrayList<>();
+        line.words().forEach(words::add);
+
+        Assert.assertThat(words.size(), is(equalTo(2)));
+        Assert.assertThat(words.get(0), is(equalTo("Hello,")));
+        Assert.assertThat(words.get(1), is(equalTo("World!")));
+    }
+
+    @Test
+    public void singleWordLineConvertedTest() {
+        final String originalContent = "Hello \t\n \r\t \n\n\n \t\r \r\n";
+        final Line line = new Line(originalContent);
+
+        final List<String> words = new ArrayList<>();
+        line.words().forEach(words::add);
+
+        Assert.assertThat(words.size(), is(equalTo(1)));
+        Assert.assertThat(words.get(0), is(equalTo("Hello")));
+    }
+}
