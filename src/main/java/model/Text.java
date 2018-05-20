c1a3b8be36c2dcaa3aeed83f680b3e11b7277ed1
nErumin <nerumin90@gmail.com>
2018-05-20 16:01:52 +0900

    Text 클래스의 주석을 수정함.

--- src/main/java/model/Text.java
+++ src/main/java/model/Text.java
@@ -11,7 +11,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 /**
- * 텍스트의 한 문장을 나타내는 클래스입니다.
+ * 텍스트를 나타내는 클래스입니다.
  */
 public final class Text {
     private static final String WORD_SPLIT_REGEX = "\\s*\\S+\\s*";
@@ -30,14 +30,14 @@ public final class Text {
     private StringBuffer buffer;
 
     /**
-     * 빈 내용을 가지는 문장을 생성합니다.
+     * 빈 내용을 가지는 텍스트를 생성합니다.
      */
     public Text() {
         this(StringUtility.EMPTY_STRING);
     }
 
     /**
-     * 초기 내용을 가지는 문장을 생성합니다.
+     * 초기 내용을 가지는 텍스트를 생성합니다.
      * @param text 초기 내용으로 설정할 문자열입니다.
      * @throws NullPointerException 매개변수가 null이면 발생합니다.
      */
@@ -50,8 +50,8 @@ public final class Text {
     }
 
     /**
-     * 이 문장에 존재하는 단어에 대한 열거자를 가져옵니다.
-     * @return 문장에 존재하는 단어에 대한 열거자입니다.
+     * 이 텍스트에 존재하는 단어에 대한 열거자를 가져옵니다.
+     * @return 텍스트에 존재하는 단어에 대한 열거자입니다.
      */
     public Iterable<String> words() {
         return StreamUtility.toIterable(wordStream());
@@ -64,8 +64,8 @@ public final class Text {
     }
 
     /**
-     * 이 문장에서 공백 문자를 포함하는 단어에 대한 열거자를 가져옵니다.
-     * @return 문장에 존재하는 공백 문자 포함 단어들의 열거자입니다.
+     * 이 텍스트에서 공백 문자를 포함하는 단어에 대한 열거자를 가져옵니다.
+     * @return 텍스트에 존재하는 공백 문자 포함 단어들의 열거자입니다.
      */
     public Iterable<String> spaceIncludingWords() {
         return StreamUtility.toIterable(spaceIncludingWordStream());
@@ -88,7 +88,7 @@ public final class Text {
      * @param length 시작 위치에서 가져올 길이를 나타냅니다.
      * @return 주어진 범위의 부분 텍스트를 나타냅니다.
      * @throws StringIndexOutOfBoundsException
-     * startIndex가 음수거나, 문장의 길이를 벗어나면 발생합니다.
+     * startIndex가 음수거나, 텍스트의 길이를 벗어나면 발생합니다.
      * @throws IllegalArgumentException length가 음수면 발생합니다.
      */
     public Text subText(int startIndex, int length) {
@@ -110,7 +110,7 @@ public final class Text {
     }
 
     /**
-     * 문장의 특정 위치에 문자열을 삽입합니다.
+     * 텍스트의 특정 위치에 문자열을 삽입합니다.
      * @param insertionPos 문자열을 삽입할 위치입니다.
      * @param insertedString 삽입할 문자열입니다.
      * @throws StringIndexOutOfBoundsException 위치가 유효하지 않으면 발생합니다.
@@ -129,11 +129,11 @@ public final class Text {
     }
 
     /**
-     * 문장의 특정 영역을 지웁니다.
+     * 텍스트의 특정 영역을 지웁니다.
      * @param startIndex 삭제를 시작할 위치입니다.
      * @param length 삭제할 영역의 길이를 나타냅니다.
      * @throws StringIndexOutOfBoundsException
-     *  startIndex가 음수거나, 문장의 길이를 벗어나면 발생합니다.
+     *  startIndex가 음수거나, 텍스트의 길이를 벗어나면 발생합니다.
      * @throws IllegalArgumentException length가 음수면 발생합니다.
      */
     public void delete(int startIndex, int length) {
@@ -149,11 +149,11 @@ public final class Text {
     }
 
     /**
-     * 문장의 특정 영역을 교체합니다.
-     * @param startIndex 교체를 시작할 문장 내 위치입니다.
+     * 텍스트의 특정 영역을 교체합니다.
+     * @param startIndex 교체를 시작할 텍스트 내 위치입니다.
      * @param replacingString 해당 위치부터 교체할 문자열입니다.
      * @throws StringIndexOutOfBoundsException
-     *  startIndex가 음수거나, 문장의 길이를 벗어나면 발생합니다.
+     *  startIndex가 음수거나, 텍스트의 길이를 벗어나면 발생합니다.
      */
     public void replace(int startIndex, String replacingString) {
         if (startIndex < 0 || startIndex >= length()) {
@@ -168,7 +168,7 @@ public final class Text {
     }
 
     /**
-     * 이 문장의 끝에 새로운 내용을 추가합니다.
+     * 이 텍스트의 끝에 새로운 내용을 추가합니다.
      * @param addedString 새롭게 추가할 내용입니다.
      * @throws NullPointerException 매개변수가 null이면 발생합니다.
      */
@@ -181,8 +181,8 @@ public final class Text {
     }
 
     /**
-     * 문장의 길이를 반환합니다.
-     * @return 문장의 길이입니다.
+     * 텍스트의 길이를 반환합니다.
+     * @return 텍스트의 길이입니다.
      */
     public int length() {
         return buffer.length();
@@ -214,8 +214,8 @@ public final class Text {
     }
 
     /**
-     * 문장 전체를 문자열로 반환합니다.
-     * @return 문장 전체의 정보입니다.
+     * 텍스트 전체를 문자열로 반환합니다.
+     * @return 텍스트 전체의 정보입니다.
      */
     @Override
     public String toString() {
