32643280e6ec2ce834b5f1df58ab333465a1c92f
kjw0412c <kjw0412c@gmail.com>
2018-05-29 03:05:13 +0900

    Merge Class 선언
    메소드 선언 및 멤버변수 선언

---
+++ src/main/java/Merge.java
@@ -0,0 +1,15 @@
+
+public class Merge {
+	String[] Compare;
+	String[] Origin;
+	int changeIndex;
+	
+	Merge(){
+		
+	}
+	 
+	void changeLine(int changeIndex) {
+		Origin[changeIndex] = Compare[changeIndex];
+	}
+	
+}
