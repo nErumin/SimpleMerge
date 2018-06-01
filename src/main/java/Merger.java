c8d5da9d9dc93d16398137e8829b84c07ed252bd
JunHyuckWoo <wjh0470@gmail.com>
2018-06-01 15:42:11 +0900

    Add Merger

---
+++ src/main/java/Merger.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Merger {
+    ArrayList<String> left = new ArrayList<String>(),
+        right = new ArrayList<String>();
+    int changeIndex;
+
+    Merger(){
+
+    }
+
+    void mergeLeft2Right() {
+
+    }
+
+    void mergeRight2Left() {
+
+    }
+
+}
