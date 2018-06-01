ef12a2d1042f7c17a37dbe1c89178b1a7a20dad3
nErumin <nerumin90@gmail.com>
2018-06-01 16:31:45 +0900

    Iterable에 대한 유틸리티 추가/주석 보완.

--- src/main/java/utility/IterableUtility.java
+++ src/main/java/utility/IterableUtility.java
@@ -7,10 +7,32 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class IterableUtility {
+    /**
+     * Iterable을 Stream으로 변환합니다.
+     * @param iterable 변환할 Iterable입니다.
+     * @return 생성된 Stream입니다.
+     */
     public static <T> Stream<T> toStream(Iterable<T> iterable) {
         return StreamSupport.stream(iterable.spliterator(), false);
     }
 
+    /**
+     * Iterable 요소를 List로 변환합니다.
+     * @param iterable 변환할 Iterable입니다.
+     * @return 변환된 List입니다.
+     */
+    public static <T> List<T> toList(Iterable<T> iterable) {
+        List<T> list = new ArrayList<>();
+        iterable.forEach(list::add);
+        return list;
+    }
+
+    /**
+     * Iterable 요소에서 조건을 만족하는 요소를 골라냅니다.
+     * @param iterable 요소들이 들어있는 Iterable입니다.
+     * @param condition 요소가 만족해야 하는 조건을 나타냅니다.
+     * @return 조건을 만족하는 요소로만 이루어진 Iterable입니다.
+     */
     public static <T> Iterable<T> filter(Iterable<T> iterable,
                                          Predicate<T> condition) {
         return StreamUtility.toIterable(
