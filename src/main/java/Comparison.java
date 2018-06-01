774a468d75719a7beef6f09a80698d07cae11fbd
JunHyuckWoo <wjh0470@gmail.com>
2018-06-01 16:43:31 +0900

    Commit After pull

--- src/main/java/Comparison.java
+++ src/main/java/Comparison.java
@@ -160,7 +160,7 @@ public class Comparison {
     public ArrayList<String> panelFix(ArrayList<String> leftPanelList, ArrayList<String> rightPanelList) {
         int howMany; lcsPanelLength(leftPanelList, rightPanelList);
         int[][] pair = solPair;
-        
+
         for (int i = 0; i < pairNum; i++) {
             if (pair[i][0] > pair[i][1]) {
                 howMany = pair[i][0] - pair[i][1];
